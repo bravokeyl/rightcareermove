6abedb88ff38b9f88da258d10088f2e4c669a67f
bravokeyl <bravokeyl@gmail.com>
2017-06-14 00:41:37 +0530

    Add font awesome

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@
  * @since Jobify Classic 1.0.0
  */
 function jobify_child_styles() {
+    wp_enqueue_style( 'fontawesome', get_theme_file_uri("/fa/font-awesome.min.css") );
     wp_enqueue_style( 'jobify-child', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'jobify_child_styles', 20 );
